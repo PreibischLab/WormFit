475c5802aedd42cb70a6b86bee5f4c0cb8c6e6a0
milkyklim <klim.kolyvanov@gmail.com>
2016-07-27 10:34:40 +0200

    current version has a bug/fails if the kernel size is even! that's it!

--- src/main/java/deconvolution/PreProcessImage.java
+++ src/main/java/deconvolution/PreProcessImage.java
@@ -48,7 +48,7 @@ public class PreProcessImage
 		Img< FloatType > img = ImgLib2Util.openAs32Bit( new File( "src/main/resources/decon/dros-1-pb.tif" ) ); //psi_synthetic.tif" ) );
 		AdjustInput.adjustImage( ImgLib2.wrapFloatToImgLib1( img ), LucyRichardson.minValue, 1 );
 		Img< FloatType > convImg = img.copy();
-		Img< FloatType > psf = ImgLib2Util.openAs32Bit( new File( "src/main/resources/decon/psf-shift.tif" ) );
+		Img< FloatType > psf = ImgLib2Util.openAs32Bit( new File( "src/main/resources/decon/psf-cross-big100.tif" ) );
 		AdjustInput.normImage( ImgLib2.wrapFloatToImgLib1( psf ) );
 
 		FFTConvolution< FloatType > conv = new FFTConvolution< FloatType >( convImg, psf );
