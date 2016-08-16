d7019ab9ccdff9cc6853fefdd9c34c64bf557623
milkyklim <klim.kolyvanov@gmail.com>
2016-08-16 11:31:04 +0200

    working parallel version

--- src/main/java/weka/test/WekaTest.java
+++ src/main/java/weka/test/WekaTest.java
@@ -10,6 +10,7 @@ import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
+import spim.process.fusion.FusionHelper;
 import weka.classifiers.Classifier;
 import weka.classifiers.trees.J48;
 import weka.classifiers.trees.RandomForest;
@@ -18,16 +19,16 @@ import trainableSegmentation.*;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import hr.irb.fastRandomForest.FastRandomForest;
 import ij.ImageJ;
 import ij.ImagePlus;
-
-
 
 public class WekaTest {
 
@@ -37,6 +38,8 @@ public class WekaTest {
 		final RandomAccessibleInterval< T > dst;
 		final int n;
 		final WekaSegmentation segmentator;
+		
+		
 
 		public processThread( final ImagePortion task, final RandomAccessibleInterval<T> src, final RandomAccessibleInterval< T > dst, WekaSegmentation segmentator)
 		{
@@ -49,104 +52,182 @@ public class WekaTest {
 
 		@Override
 		public Void call() throws Exception{
-			
-			ImagePlus imp = ImageJFunctions.wrap(src, "");
-			
-			
-			// final Cursor<T> cSrc = Views.iterable(src).localizingCursor();
-			// final RandomAccess<T> rDst = dst.randomAccess();
+			try
+			{
+				ImagePlus out1 = segmentator.applyClassifier(ImageJFunctions.wrap(Views.interval(src, task.min, task.max), ""), 0, false);
+				// Display classified image
+				out1.setTitle("");
+				out1.show();
 
-			// cSrc.jumpFwd(task.getStartPosition());
-
-			// do something with image 			
-			for (int j = 0; j < task.loopSize; ++j){
-				cSrc.fwd();
-				
-				// TODO: the segmentation should come here 
-				
-				
-				
-				
-				rDst.setPosition(cSrc);
-				rDst.get().set(cSrc.get());
+				// testFunction();
+			}
+			catch (Exception e ) {
+				System.out.println(task.toString());
+				e.printStackTrace();
 			}
 
 			return null;
 		} 		
+
+		// simply the test function 
+		// сhecks whether all threads are running
+		public void testFunction(){
+			final Cursor<T> cSrc = Views.interval(src, task.min, task.max).localizingCursor();
+			final RandomAccess<T> rDst = dst.randomAccess();
+
+			System.out.println("Called!");
+			// cSrc.jumpFwd(task.getStartPosition());
+			while(cSrc.hasNext()){
+				cSrc.fwd();
+				rDst.setPosition(cSrc);
+				rDst.get().set(cSrc.get());
+			}
+		}
 	}
+
+
+
 
 	public static class ImagePortion
 	{
-		public ImagePortion( final long startPosition, long loopSize )
-		{
-			this.startPosition = startPosition;
-			this.loopSize = loopSize;
+
+		public ImagePortion(final long[] min, final long[] max){
+
+			// allocate memory
+			this.min = new long[min.length];
+			this.max = new long[max.length];
+
+			for (int i = 0; i < min.length; ++i){
+				this.min[i] = min[i];
+				this.max[i] = max[i];
+			}
 		}
 
-		public long getStartPosition() { return startPosition; }
-		public long getLoopSize() { return loopSize; }
+		// write the min values to result
+		public void getMin(long [] result){
+			for (int i = 0; i < min.length; ++i){
+				result[i] = this.min[i];
+			}
+		}
 
-		protected long startPosition;
-		protected long loopSize;
+		// write the min values to result
+		public void getMax(long [] result){
+			for (int i = 0; i < max.length; ++i){
+				result[i] = this.max[i];
+			}
+		}
+
+		// set min value for the provided dimension
+		public void setMin(int d, long val){
+			min[d] = val;
+		}
+
+		// set min value for the provided dimension
+		public void setMax(int d, long val){
+			max[d] = val;
+		}		
+
+
+		protected long[] min;
+		protected long[] max;
 
 		@Override
-		public String toString() { return "Portion [" + getStartPosition() + " ... " + ( getStartPosition() + getLoopSize() - 1 ) + " ]"; }
+		public String toString() { return "Portion [" + min[0] + " ... " + max[0] + " ]" +  "[" + min[1] + " ... " + max[1] + " ]"; }
 	}
 
-	public static final Vector<ImagePortion> divideIntoPortions( final long imageSize, final int numPortions )
-	{
-		final long threadChunkSize = imageSize / numPortions;
-		final long threadChunkMod = imageSize % numPortions;
+
+
+	public static final Vector<ImagePortion> divideIntoPortions( final long[] dimensions, final int [] numPortions){
+		
+		final long[] threadChunkSize = new long[dimensions.length];
+		final long[] threadChunkMod = new long[dimensions.length];
+
+		for (int d = 0; d < dimensions.length; ++d){
+			// TODO: quick fix to make the code scalable / rewrite
+			threadChunkSize[d] = (d == 0 ? dimensions[d] / numPortions[d] : dimensions[d]);
+			threadChunkMod[d] =  (d == 0 ? dimensions[d] % numPortions[d] : 0            );			
+		}
 
 		final Vector<ImagePortion> portions = new Vector<ImagePortion>();
 
-		for ( int portionID = 0; portionID < numPortions; ++portionID )
-		{
-			// move to the starting position of the current thread
-			final long startPosition = portionID * threadChunkSize;
+		long [] min = new long[dimensions.length];
+		long [] max = new long[dimensions.length];	
 
-			// the last thread may has to run longer if the number of pixels cannot be divided by the number of threads
-			final long loopSize;
-			if ( portionID == numPortions - 1 )
-				loopSize = threadChunkSize + threadChunkMod;
-			else
-				loopSize = threadChunkSize;
-
-			portions.add( new ImagePortion( startPosition, loopSize ) );
+		for (int d = 1; d < dimensions.length; ++d){
+			min[d] = 0;
+			max[d] = dimensions[d] - 1;
 		}
+
+		// create all portions without coordinates
+		// TODO: useful
+		//		for (int d = 0; d < dimensions.length; ++d){
+		//			for(int portionID = 0; portionID < numPortions[d]; ++portionID){
+		//				portions.add( new ImagePortion( new long[dimensions.length], new long[dimensions.length] ) );
+		//			}
+		//		}
+
+		// iterate only over x-axis (we currently take the full slice of the image)
+		for(int portionID = 0; portionID < numPortions[0]; ++portionID){
+			// move to the starting position of the current thread
+			min[0] = portionID * threadChunkSize[0];
+			// the last thread may has to run longer if the number of pixels cannot be divided by the number of threads
+			if ( portionID == numPortions[0] - 1 )
+				max[0] = threadChunkSize[0]*(portionID + 1) + threadChunkMod[0] - 1;
+			else
+				max[0] = threadChunkSize[0]*(portionID + 1) - 1;
+
+			portions.addElement(new ImagePortion(min, max));
+		}
+
 
 		return portions;
 	}
 
 	public static <T extends RealType<T>> void runParallelMethod(RandomAccessibleInterval<T> img, RandomAccessibleInterval<T> dst){
 		int numThreads = 4;
-		int numTasks = 16; 
-		// TODO: change numTasks to an array of chunks 
-		
+		int [] numTasks = new int[img.numDimensions()];
+
 		// Weka code // Sequential
+		// TODO:  remove? 
 		ImagePlus imp = ImageJFunctions.wrap(img, "");
-		WekaSegmentation segmentator = new WekaSegmentation(); 
+		WekaSegmentation segmentator = new WekaSegmentation(imp); 
 		
+		segmentator.getMembranePatchSize(); // this one should be included in the size of the image
+
 		boolean isLoaded = segmentator.loadClassifier("src/main/resources/classifier.model"); // OK! 
 		if (!isLoaded){
 			System.out.println("Problem loading classifier");
-			return;
+			// return;
 		}
-		
-		
+		else{
+			System.out.println("Loaded!");
+		}
+
 		// @Parallel : 
 		final ExecutorService taskExecutor = Executors.newFixedThreadPool( numThreads );
 		final ArrayList< Callable<Void> > taskList = new ArrayList< Callable< Void > >(); 
 
+		long [] dimensions = new long[img.numDimensions()];
+		for (int d = 0; d < img.numDimensions(); ++d){
+			dimensions[d] = img.dimension(d);
+			numTasks[d] = 1; 
+		}
+
+		// split only x axis
+		numTasks[0] = 3;
+
 		// @Parallel :
-		for (final ImagePortion task : divideIntoPortions(Views.iterable(img).size(), numTasks) )
+		for (final ImagePortion task : divideIntoPortions(dimensions, numTasks) ){
 			taskList.add(new processThread< T >( task, img, dst, segmentator));
+			// System.out.println("taskes created");
+		}
 
 		try
 		{
 			// invokeAll() returns when all tasks are complete
 			// synchronization point
 			taskExecutor.invokeAll(taskList);
+
 		}
 		catch(final Exception e){
 			System.out.println( "Failed to invoke all tasks" + e );
