79093c469467e5b6aaea6c26f41fd29716871b93
milkyklim <klim.kolyvanov@gmail.com>
2016-08-30 17:00:39 +0200

    current state. defines the center of beads correctly

--- src/main/java/klim/DeconvolutionTest.java
+++ src/main/java/klim/DeconvolutionTest.java
@@ -1,8 +1,12 @@
 package klim;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import ij.ImageJ;
+import net.imglib2.Cursor;
+import net.imglib2.Point;
+import net.imglib2.PointSampleList;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.stats.Normalize;
 import net.imglib2.img.Img;
@@ -14,6 +18,7 @@ import net.imglib2.type.logic.BitType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.view.Views;
 import klim.Thresholding;
 import util.ImgLib2Util;
 
@@ -21,23 +26,70 @@ public class DeconvolutionTest {
 	
 	public static <T extends RealType<T>> void getPsf(RandomAccessibleInterval<T> img, RandomAccessibleInterval<BitType> out, RandomAccessibleInterval<T> psf){
 		T tVal = img.randomAccess().get();
-		tVal.setReal(128f);
+		tVal.setReal(70);
 		Thresholding.threshold(img, out, tVal);
 		
-		final ImgLabeling<Integer, IntType> labeling = new ImgLabeling<Integer, IntType>(new ArrayImgFactory<IntType>().create(out, new IntType())); 
+		// ArrayList<ObjectSegmentation> beadsList = new ArrayList<ObjectSegmentation>();
+		
+		final ImgLabeling<Integer, IntType> labeling = new ImgLabeling<Integer, IntType>(new ArrayImgFactory<IntType>().create(out, new IntType())); 		
 		ObjectSegmentation.setLabeling(out, labeling);
-		ObjectSegmentation.setParameters(out, labeling);
+		PointSampleList<T> beads = new PointSampleList<T>(img.numDimensions());
+		// long numBeads = 0; // -1 ? 
+		// System.out.println(labeling.size());
+		
+		ObjectSegmentation.findBeads(img, labeling, beads);
+		// beads 0 is the background!
+		
+		
+		
+		
+		// ArrayList<ObjectSegmentation> beadsList = ObjectSegmentation.setParameters(out, labeling);
 		// @DEBUG: 
-		ImageJFunctions.show(labeling.getIndexImg());
+		// ImageJFunctions.show(out);
 		// 
+		// ObjectSegmentation.removeNoise(beads, img);
+		
+		// PointSampleList<T> beads = new PointSampleList<T>(img.numDimensions());
+		// tVal.setReal(100); // if above this value, probably bead center
+		// detectBeads(img, beads, tVal);
+		
+		Cursor<T> cursor = beads.cursor();
+		while(cursor.hasNext()){
+			long[] min = new long[3];
+			long[] max = new long[3];
+			
+			cursor.fwd();
+			for (int d = 0; d < img.numDimensions(); d++){
+				System.out.print("[" + (cursor.getLongPosition(d) - 10) + " " + (cursor.getLongPosition(d) + 10) + "]");
+				min[d] = cursor.getLongPosition(d) - 10;
+				max[d] = cursor.getLongPosition(d) + 10;
+			}
+			System.out.println();
+			
+			
+			
+			ImageJFunctions.show(Views.rotate(Views.interval(img, min, max), 0, 2));
+		}
 		
 	}
 	
+	public static <T extends RealType<T> & Comparable<T>> void detectBeads(RandomAccessibleInterval<T> img, PointSampleList<T> beads, T tVal){
+		Cursor<T> cursor  = Views.iterable(img).cursor();
+		
+		while(cursor.hasNext()){
+			cursor.fwd();
+			if (cursor.get().compareTo(tVal) > 0 ){
+				beads.add(new Point(cursor), cursor.get());
+			}
+		}
+	}
 	
 	
 	public static void main(String[] args){
 		// TODO: add the correct pic here
-		Img< FloatType > img = ImgLib2Util.openAs32Bit( new File( "src/main/resources/inputTest3Dsmall.tif" ) ); 
+		// Img< FloatType > img = ImgLib2Util.openAs32Bit( new File( "src/main/resources/inputTest3Dsmall.tif" ) ); 
+		Img< FloatType > img = ImgLib2Util.openAs32Bit( new File( "../Desktop/beeds.tif" ) ); 
+		
 		Img< BitType > out = new ArrayImgFactory<BitType>().create(img, new BitType());
 		// TODO: check
 		float minValue =  0;
@@ -47,7 +99,7 @@ public class DeconvolutionTest {
 		getPsf(img, out, img);
 		
 		new ImageJ();
-		// ImageJFunctions.show(img).setTitle("Initial Image");
+		ImageJFunctions.show(img).setTitle("Initial Image");
 		// ImageJFunctions.show(out).setTitle("Thresholded Image");
 		
 		System.out.println("Doge!");
--- src/main/java/klim/ObjectSegmentation.java
+++ src/main/java/klim/ObjectSegmentation.java
@@ -22,51 +22,100 @@ import net.imglib2.type.numeric.integer.IntType;
 import net.imglib2.view.Views;
 
 public class ObjectSegmentation {
-	
+
 	private final long[] min; 
 	private final long[] max; 
 	private final long[] size;
 	private long index;
-	
+	private int numDimensions;
+
 	public ObjectSegmentation(long[] min, long[] max, long[] size, long index){
+		this.numDimensions = min.length;
 		this.min = new long[min.length];
 		this.max = new long[max.length];
 		this.size = new long[size.length];
 		this.index = index;
-		
+
 		for (int d = 0; d < min.length; d++){
 			this.min[d] = min[d];
 			this.max[d] = max[d];
 			this.size[d] = size[d];
 		}
 	}
-	
+
 	// returns labeling for each connected component 
 	public static <T extends RealType<T>> void setLabeling(RandomAccessibleInterval<BitType> bitImg, ImgLabeling<Integer, IntType> labeling) {
 		final Iterator<Integer> labelIterator = AllConnectedComponents.getIntegerNames(0);
 		ConnectedComponents.labelAllConnectedComponents(bitImg, labeling, labelIterator, ConnectedComponents.StructuringElement.EIGHT_CONNECTED );		
 	}
 	
-	public static <T extends RealType<T>> void setParameters(final RandomAccessible<T> img, final ImgLabeling<Integer, IntType> labeling){
-		
-		ArrayList<ObjectSegmentation> objects = new ArrayList<ObjectSegmentation>();
-		
+	public static int getMaxLabel(final ImgLabeling<Integer, IntType> labeling){
+		Cursor<IntType> cursor = Views.iterable(labeling.getIndexImg()).cursor();
+		int maxLabel = 0;
+		while(cursor.hasNext()){
+			cursor.fwd();
+			if(cursor.get().get() > maxLabel)
+				maxLabel = cursor.get().get();
+		}
+		return maxLabel;
+	} 
+
+	public static <T extends RealType<T>> void findBeads(final RandomAccessibleInterval<T> img, final ImgLabeling<Integer, IntType> labeling, PointSampleList<T> beads){
 		Cursor<IntType> cursor = Views.iterable(labeling.getIndexImg()).cursor();
 		RandomAccess <T> randomAccess = img.randomAccess();
+
+		int numBeads = 0;
+		numBeads = getMaxLabel(labeling) + 1;
 		
+		System.out.println(numBeads);
+		long[][] averageBeadsCoordinates = new long[numBeads][randomAccess.numDimensions()];
+		long[] numPossibleBeads = new long[numBeads];
+
+		while(cursor.hasNext()){
+			cursor.fwd();
+			int idx = cursor.get().getInteger();
+			for (int d = 0; d < img.numDimensions(); d++){
+				averageBeadsCoordinates[idx][d] += cursor.getLongPosition(d);
+			}	
+			numPossibleBeads[idx]++;
+		}
+
+//		for (int j = 0; j < numBeads; ++j){
+//			System.out.print(numPossibleBeads[j] + " ");
+//		}
+		
+		System.out.println();
+		for (int j = 0; j < numBeads; ++j){
+			for (int d = 0; d < img.numDimensions(); d++){
+				averageBeadsCoordinates[j][d] /= numPossibleBeads[j];
+				System.out.print(averageBeadsCoordinates[j][d] + " ");
+			}
+			beads.add(new Point(averageBeadsCoordinates[j]), randomAccess.get());
+			System.out.println();
+		}
+		
+	}
+
+	public static <T extends RealType<T>> ArrayList<ObjectSegmentation> setParameters(final RandomAccessible<T> img, final ImgLabeling<Integer, IntType> labeling){
+
+		ArrayList<ObjectSegmentation> objects = new ArrayList<ObjectSegmentation>();
+
+		Cursor<IntType> cursor = Views.iterable(labeling.getIndexImg()).cursor();
+		RandomAccess <T> randomAccess = img.randomAccess();
+
 		// long[] dummy = new long[randomAccess.numDimensions()];
-		
+
 		long curNum = 0;
 		while(cursor.hasNext()){
-			
+
 			long[] max = new long[randomAccess.numDimensions()];
 			long[] min = new long[randomAccess.numDimensions()];
-			
+
 			for (int d = 0; d < img.numDimensions(); d++){
 				max[d] = 0;
 				min[d] = Long.MAX_VALUE;
 			}
-			
+
 			cursor.fwd();
 			long curElement = cursor.get().get();
 			// to prevent crashes
@@ -76,85 +125,82 @@ public class ObjectSegmentation {
 					++curNum; 
 				}
 			}
+			// TODO: switch to if else
 			try{
 				long [] curPosition = new long[randomAccess.numDimensions()];
 				cursor.localize(curPosition);
 				int index = cursor.get().get(); 
-				
+
 				for (int d = 0; d < img.numDimensions(); d++){
 					if (objects.get(index).max[d] < curPosition[d]){
 						objects.get(index).max[d] = curPosition[d];
 					}
-					
+
 					if (objects.get(index).min[d] > curPosition[d]){
 						objects.get(index).min[d] = curPosition[d];
 					}
-				}
-//				
-				
+				}			
 			}
 			catch(Exception e){
 				System.out.println("Lol! You failed!");
 			}
 		}
-		
-		for (ObjectSegmentation obj : objects){
-			for (int d = 0; d < img.numDimensions(); d++){
+
+		// DEBUG:
+		//		for (ObjectSegmentation obj : objects){
+		//			for (int d = 0; d < img.numDimensions(); d++){
+		//				System.out.print("[" + obj.min[d] + " " + obj.max[d] + "]");
+		//			}
+		//			System.out.println();
+		//			// ImageJFunctions.show(Views.interval(img, obj.min, obj.max));
+		//		}
+
+
+
+		// TODO: make a function
+		for (int j = 0; j < objects.size(); ++j){
+			for (int d = 0; d < objects.get(j).numDimensions; ++d){
+				objects.get(j).size[d] = objects.get(j).max[d] - objects.get(j).min[d];
+				if (objects.get(j).size[d] < 0)
+					System.out.println("Size value is negative!");
+			}
+		}
+
+		System.out.println("Size: " + objects.size());
+
+		// TODO: rewrite as a proper function
+		objects.remove(0); // drop background
+		return objects;
+
+	}
+
+	public static <T extends RealType<T>>void removeNoise(ArrayList<ObjectSegmentation> beads, RandomAccessibleInterval<T> img){
+		System.out.println("Denoizing");
+
+		int numDimensions = beads.get(0).numDimensions;
+		for (int j = 0; j < beads.size(); ++j){
+			long [] position = new long[numDimensions];
+			boolean isNoise = false; 
+			for (int d = 0; d < beads.get(j).numDimensions; ++d){
+				if(beads.get(j).size[d] <= 4)
+					isNoise = true;
+			}
+			if (isNoise){
+				beads.remove(j);
+				j--;
+			}
+			// System.out.println(j);
+		}
+
+		for (ObjectSegmentation obj : beads){
+			for (int d = 0; d < numDimensions; d++){
 				System.out.print("[" + obj.min[d] + " " + obj.max[d] + "]");
 			}
 			System.out.println();
+			ImageJFunctions.show(Views.rotate(Views.interval(img, obj.min, obj.max), 0, 2));
 		}
-		
-		
+
+
 	}
-	
-	public static <T extends RealType<T>> void segmentImage(final ImgLabeling<Integer, IntType> labeling, final RandomAccessible<T> img){
-		// this one bellow is the final list you'll need 
-		ArrayList<PointSampleList<T>> objectsList = new ArrayList<>();
 
-		Cursor<IntType> cursor = Views.iterable(labeling.getIndexImg()).cursor();
-		RandomAccess <T> randomAccess = img.randomAccess();
-
-		// number of objects in the picture calculated dynamically
-		// this part divides pixels into chunks
-		int curMax = 0;
-		while(cursor.hasNext()){
-			cursor.fwd();
-			int curElement = cursor.get().get();
-			// increase the size of the objects list
-			if (curElement >= curMax){
-				while(curElement >= curMax){
-					objectsList.add(new PointSampleList<T>(img.numDimensions()));
-					++curMax; 
-				}
-			}
-			try{
-				randomAccess.setPosition(cursor);
-				objectsList.get(curElement).add(new Point(randomAccess), randomAccess.get()); // .copy here ?
-			}
-			catch(Exception e){
-				System.out.println(objectsList.get(curElement).size());
-			}
-		}
-		
-		
-		// search for worm index 
-		// taking into account that worm is the largest object
-//		int idx = 0; 
-//		long maxSize = 0;
-//		// skip background i == 0 
-//		for (int i = 1; i < objectsList.size(); ++i){
-//			if (objectsList.get(i).size() > maxSize){
-//				maxSize = objectsList.get(i).size();
-//				idx = i;
-//			}			
-//		}		
-
-		// copy the output
-//		Cursor<T> it = objectsList.get(idx).cursor(); // copy worm
-//		while(it.hasNext()){
-//			it.fwd();
-//			worm.add(new Point(it), it.get());
-//		}
-	}
 }
