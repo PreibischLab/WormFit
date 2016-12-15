4d317770becc3051e7f2a0be900b80960450084b
milkyklim <klim.kolyvanov@gmail.com>
2016-12-15 18:52:08 +0100

    working non rigid transfomration with c.elegans

--- src/main/java/coherent/point/drift/CoherentPointDrift.java
+++ src/main/java/coherent/point/drift/CoherentPointDrift.java
@@ -1,6 +1,7 @@
 package coherent.point.drift;
 
 import java.awt.Color;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -13,6 +14,7 @@ import org.la4j.Vectors;
 import org.la4j.linear.GaussianSolver;
 import org.la4j.linear.LeastSquaresSolver;
 import org.la4j.matrix.dense.Basic2DMatrix;
+import org.la4j.operation.MatrixVectorOperation;
 import org.la4j.vector.VectorFactory;
 import org.la4j.vector.dense.BasicVector;
 import org.la4j.vector.functor.VectorAccumulator;
@@ -32,46 +34,49 @@ import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import test.TestGauss3d;
+import util.ImgLib2Util;
 import util.opencsv.CSVReader;
 import util.opencsv.CSVWriter;
 
 public class CoherentPointDrift {
-	
+
 	Img<FloatType> img;
 
 	int D = 2; // dimensionality of the point set
-	int N = 91; // # of points in the first point set
-	int M = 91; // # of points in the second point set
-	double [] X; // first point set N x D
-	double [] Y; // second point set M x D
-	double [] I; // (in theory) identity matrix
+	int N = 133; // # of points in the first point set
+	int M = 112; // # of points in the second point set
+	double[] X; // first point set N x D
+	double[] Y; // second point set M x D
+	double[] I; // (in theory) identity matrix
 
-	double [] W; // matrix of coefficients M x D 
+	double[] W; // matrix of coefficients M x D
 	double sigma2; // TODO: 2 for squared
 
-	double [] G; 
-
-	double [] P; // probability matrix 
+	double[] G;
+	double[] P; // probability matrix
 
 	// parameters
 	double w; // amount of noise [0, 1]
-	double beta; // Briefly speaking, parameter   defines the model of the smoothness regularizer (width of smoothing Gaussian filter in (20)).
-	double lambda; //Parameter   represents the trade-off between the goodness of maximum likelihood fit and regularization.
+	double beta; // Briefly speaking, parameter defines the model of the
+	// smoothness regularizer (width of smoothing Gaussian
+	// filter in (20)).
+	double lambda; // Parameter represents the trade-off between the goodness of
+	// maximum likelihood fit and regularization.
 
 	// TODO: convert stuff to the HPC
 	Basic2DMatrix mX; // N D
-	Basic2DMatrix mY; // M D 
-	Basic2DMatrix mI; // 
-	Basic2DMatrix mW; // M D 
+	Basic2DMatrix mY; // M D
+	Basic2DMatrix mI; //
+	Basic2DMatrix mW; // M D
 	Basic2DMatrix mG; // M M
 	Basic2DMatrix mP; // M N
 
-	Basic2DMatrix mT; // translation (?) transformation matrix
+	Basic2DMatrix mT; // M D transformation matrix
 
 	// straight to the non-rigid registration
 
 	// constructor
-	public CoherentPointDrift(){
+	public CoherentPointDrift() {
 	}
 
 	/**
@@ -82,94 +87,132 @@ public class CoherentPointDrift {
 		public double evaluate(int i, double value) {
 			double eps = 0.000000001;
 			double res = 0;
-			if(value < eps ){
-				System.out.println(i  + "'th value is too small to invert!");
-			}
-			else{
-				res = 1/value;
+			if (value < eps) {
+				System.out.println(i + "'th value is too small to invert!");
+			} else {
+				res = 1 / value;
 			}
 			return res;
 		}
 	};
 
-
-	public static void debugResult(Matrix m, int idx){
-		for (int j = 0; j < 20; ++j){				
-			double val = m.get(j, idx);			
-			System.out.println(j + " : "  + val);
-		}	
+	public static void debugResult(Matrix m, int idx) {
+		for (int j = 0; j < 20; ++j) {
+			double val = m.get(j, idx);
+			System.out.println(j + " : " + val);
+		}
 	}
 
-	public static void debugResult(Vector m){
-		for (int j = 0; j < 20; ++j){				
-			double val = m.get(j);			
-			System.out.println(j + " : "  + val);
-		}	
+	public static void debugResult(Vector m) {
+		for (int j = 0; j < 20; ++j) {
+			double val = m.get(j);
+			System.out.println(j + " : " + val);
+		}
 	}
-	
-	
+
 	// these guys are necessary for proper coordinates adjustment
-	double [] translate = new double[]{250, 250};
-	double [] scale = new double[]{100, 100};
-	double [] sigma = new double[]{3, 3};
-	
-	public void addPoints(Img<FloatType> img){
-		double [] location = new double [D];
+	double[] translate = new double[] { 250, 250 };
+	double[] scale = new double[] { 150, 150 };
+	double[] sigma = new double[] { 3, 3 };
+
+	public void addPoints(Img<FloatType> img) {
+		double[] location = new double[D];
 		// double [] sigma = new double [D];
-			
-//		for (int d = 0; d <D; ++d ){
-//			sigma[d] = 3;
-//		}
-		
-		for (int i= 0; i < N; ++i){
-			for (int d = 0; d <D; ++d ){
-				location[d] = (mX.get(i, d)*scale[d] + translate[d]);
+
+		// for (int d = 0; d <D; ++d ){
+		// sigma[d] = 3;
+		// }
+
+		for (int i = 0; i < N; ++i) {
+			for (int d = 0; d < D; ++d) {
+				location[d] = (mX.get(i, d) * scale[d] + translate[d]);
 				// System.out.print(location[d]);
 			}
-			//System.out.println();
+			// System.out.println();
 			TestGauss3d.addGaussian(img, location, sigma);
 		}
-	} 
+	}
+
+	// display output
 	
-	public void addOverlay(ImagePlus imp){
-		//	new ImageJ();
+	public void addOverlay(ImagePlus imp) {
+		// new ImageJ();
 		Overlay overlay = imp.getOverlay();
-		if (overlay == null){
-			System.out.println("addOverlay: overlay is null!");	
+		if (overlay == null) {
+			System.out.println("addOverlay: overlay is null!");
 			overlay = new Overlay();
-			imp.setOverlay( overlay );
-		}		
+			imp.setOverlay(overlay);
+		}
 		overlay.clear();
-		
+
 		// here should be the T coordinate of the fitted points
-		for (int i = 0; i  < M; ++i){
-				double [] xy = new double[D];
-				for (int d = 0; d < D; ++d){
-					xy[d] =  mT.get(i, d)*scale[d] + translate[d];
-					System.out.print(xy[d]+ " ");
-				}
-				System.out.println();
-				final OvalRoi or = new OvalRoi( xy[0]- sigma[0], xy[1] - sigma[0], Util.round( 2*sigma[0] ), Util.round( 2*sigma[1] ) );
-				or.setStrokeColor(Color.orange);
-				overlay.add(or);
+		for (int i = 0; i < M; ++i) {
+			double[] xy = new double[D];
+			for (int d = 0; d < D; ++d) {
+				xy[d] = mT.get(i, d)*scale[d] + translate[d];
+				// System.out.print(xy[d] + " ");
+			}
+			// System.out.println();
+			final OvalRoi or = new OvalRoi(xy[0] - sigma[0], xy[1] - sigma[0], Util.round(2 * sigma[0]),
+					Util.round(2 * sigma[1]));
+			or.setStrokeColor(Color.RED);
+			overlay.add(or);
 		}
 		imp.setOverlay(overlay);
-//		imp.updateAndDraw();
-		
+		// imp.updateAndDraw();
 	}
-	
-	
-	
-	
-	public int runNonRigidRegistration(int flag){
+
+	// normalize with zero mean and variance 1
+	// normalization along column
+	// TODO: this function is horrible cause it doesn't use the API of la4j
+	// but it is working!
+	public  void normalize(Matrix M){
+		double[] sumM = M.foldColumns(Vectors.asSumAccumulator(0.0));
+		// TODO: Rewrite using vectorization
+		for (int i =0; i < D; ++i){
+			sumM[i] /= M.getColumn(i).length();
+		}
+		
+		// System.out.println(sumM[0] + " " + sumM[1]);
+		
+		// TODO: Rewrite using vectorization
+		for (int i = 0; i < M.rows(); ++i){
+			for (int d = 0 ; d < D; ++d){
+				double val = M.get(i, d) - sumM[d];
+				M.set(i, d, val);
+			}	
+		}
+		
+		// debugResult(M, 0);	
+		double scaleVal = 0;
+		
+		for (int i = 0; i < M.rows(); ++i){
+			for (int d = 0; d < D; ++d){
+				scaleVal += M.get(i, d)*M.get(i, d);
+			}
+		}
+		
+		scaleVal /= (M.rows());
+		scaleVal = Math.sqrt(scaleVal);
+		System.out.println(scaleVal);
+		
+		for (int i = 0; i < M.rows(); ++i){
+			for (int d = 0 ; d < D; ++d){
+				double val = M.get(i, d)/scaleVal;
+				M.set(i, d, val);
+			}	
+		}		
+	}
+
+	public int runNonRigidRegistration(int flag) {
 		// initialization
 		// reading the files
 
 		mX = new Basic2DMatrix(N, D);
 		mY = new Basic2DMatrix(M, D);
-		mI = new Basic2DMatrix(N, D); // TODO: ?? 
+		mI = new Basic2DMatrix(N, D); // TODO: ??
 		mW = new Basic2DMatrix(M, D);
-		mG = new Basic2DMatrix(N, M);
+		mG = new Basic2DMatrix(M, M);
 		mP = new Basic2DMatrix(M, N);
 
 		mT = new Basic2DMatrix(M, D);
@@ -177,154 +220,169 @@ public class CoherentPointDrift {
 		mW.setAll(0);
 		// TODO: this assholes also do normalization for some reasons
 		readCSV(); // reading is fine
-		
+		normalize(mX);
+		normalize(mY);
+	
 		img = new ArrayImgFactory<FloatType>().create(new long[]{500, 500}, new FloatType());
 		addPoints(img);
+		// img = ImgLib2Util.openAs32Bit(new File("/Users/kkolyva/Desktop/cpd_ex/worm-folded.tif"));
+
 		// need a container because not yet implemented in I2
 		final ImagePlus imp = ImageJFunctions.wrapFloat(img, "Put an overlay on top!");
 		imp.show();
-		// ImageJFunctions.show(img);	
+		// ImageJFunctions.show(img);
 		// column-wise summation
-		double[] sumMX = mX.foldColumns(Vectors.asSumAccumulator(0.0));		
+		double[] sumMX = mX.foldColumns(Vectors.asSumAccumulator(0.0));
 		double[] sumMY = mY.foldColumns(Vectors.asSumAccumulator(0.0));
 
 		double sum = 0;
 
 		for (int d = 0; d < D; ++d)
-			sum += sumMX[d]*sumMY[d];
+			sum += sumMX[d] * sumMY[d];
 
+		sigma2 = (double) (M * mX.transpose().multiplyByItsTranspose().trace()
+				+ N * mY.transpose().multiplyByItsTranspose().trace() - 2 * sum) / (M * N * D);
 
-		sigma2 = (double) (M*mX.transpose().multiplyByItsTranspose().trace() +
-				N*mY.transpose().multiplyByItsTranspose().trace() - 2*sum) / (M*N*D);
+		System.out.println("sigma2 : " + sigma2);
+		
 
-		// System.out.println("sigma2 = " + sigma2);
-
-		w = 0.1f; 
+		
+		
+		w = 0.1f;
 		beta = 2;
 		lambda = 3;
 
-		// there might be a better way for this part 
-		for (int i = 0; i < N; ++i){
-			for (int j = 0; j < M; ++j){
-				double val = (double)mY.getRow(i).subtract(mY.getRow(j)).norm();
-				val *= -val/(2*beta*beta);
+		// there might be a better way for this part
+		for (int i = 0; i < M; ++i) {
+			for (int j = 0; j < M; ++j) {
+				double val = (double) mY.getRow(i).subtract(mY.getRow(j)).norm();
+				val *= -val / (2 * beta * beta);
 				mG.set(i, j, Math.exp(val));
 			}
 		}
 		// stuff above looks valid
-		// G looks correct at the moment 
+		// G looks correct at the moment
+		
+		
+
+		mT = (Basic2DMatrix) mT.insert(mY.add(mG.multiply(mW)));
+		addOverlay(imp);
 		
 		int counter = 0;
-		while (counter++ < 30){
+		while (counter++ < 30) {
 
 			System.out.println("ITERATION #" + counter);
 
 			// TODO: OPTIMIZE
-			for (int m = 0; m < N; ++m){
-				for (int n = 0; n < M; ++n){
-					double val = (double)mX.getRow(n).subtract(mY.getRow(m).add(mG.getRow(m).multiply(mW))).norm();
-					val *= -val/(2*sigma2);
-					val = (double)Math.exp(val);
+			for (int m = 0; m < M; ++m) {
+				for (int n = 0; n < N; ++n) {
+					double val = (double) mX.getRow(n).subtract(mY.getRow(m).add(mG.getRow(m).multiply(mW))).norm();
+					val *= -val / (2 * sigma2);
+					val = (double) Math.exp(val);
 
 					// TODO: is it possible to move this part out of the loop
 					double denom = 0;
-					for (int k = 0; k < M; ++k){
-						double tmp = (double)mX.getRow(n).subtract(mY.getRow(k).add(mG.getRow(k).multiply(mW))).norm();
-						tmp *= -tmp/(2*sigma2);
+					for (int k = 0; k < M; ++k) {
+						double tmp = (double) mX.getRow(n).subtract(mY.getRow(k).add(mG.getRow(k).multiply(mW))).norm();
+						tmp *= -tmp / (2 * sigma2);
 						denom += Math.exp(tmp);
-					}				
-					denom += w / (1 - w) * Math.pow(2 * Math.PI * sigma2, D / 2 ) * M / N; 
-					mP.set(m, n, val/denom);
+					}
+					denom += w / (1 - w) * Math.pow(2 * Math.PI * sigma2, D / 2) * M / N;
+					mP.set(m, n, val / denom);
 				}
 			}
 
-			// TODO: I think that should be N instead of M here 
+			// TODO: I think that should be N instead of M here
 			// TODO: FIXED: check if this thing is correct at all
-			BasicVector ones = BasicVector.constant(N, 1);		
+			BasicVector ones = BasicVector.constant(N, 1);
 			Vector invP = mP.multiply(ones);
-			// Up to here output is correct		
-			invP.update(INV2_FUNCTION); // you will reuse this value !!	
-			// Up to here output is correct 
+			// Up to here output is correct
+			invP.update(INV2_FUNCTION); // you will reuse this value !!
+			// Up to here output is correct
 			// debugResult(invP);
-			// if (true) return 0;			
+			// if (true) return 0;
 
-
-			// TODO: now you know that the parameters are fine! 
+			// TODO: now you know that the parameters are fine!
 			// check out the values in the left handside and right hand side
 			// after that adjust the solver
-			 
-			 // the left hand side is calculated correctly
-			 // check the right hand-side
-			 
+
+			// the left hand side is calculated correctly
+			// check the right hand-side
+
 			System.out.println(sigma2 + " : " + lambda);
-			
-			for (int d = 0; d < D; ++d){		
-				// mW.setColumn(d, new GaussianSolver(mG.add(invP.toDiagonalMatrix().multiply(sigma2 * lambda))).solve( (invP.toDiagonalMatrix().multiply(mP).multiply(mX)).subtract(mY).getColumn(d)));	
-				mW.setColumn(d, new LeastSquaresSolver(mG.add(invP.toDiagonalMatrix().multiply(sigma2 * lambda))).solve( (invP.toDiagonalMatrix().multiply(mP).multiply(mX)).subtract(mY).getColumn(d)));				
+
+			// Problem pops up after this statement
+
+			for (int d = 0; d < D; ++d) {
+				// mW.setColumn(d, new
+				// GaussianSolver(mG.add(invP.toDiagonalMatrix().multiply(sigma2
+				// * lambda))).solve(
+				// (invP.toDiagonalMatrix().multiply(mP).multiply(mX)).subtract(mY).getColumn(d)));
+				mW.setColumn(d, new LeastSquaresSolver(mG.add(invP.toDiagonalMatrix().multiply(sigma2 * lambda)))
+						.solve((invP.toDiagonalMatrix().multiply(mP).multiply(mX)).subtract(mY).getColumn(d)));
 			}
 
-			
-			
-			
-			
 			double N_P = (double) mP.sum();
-			mT = (Basic2DMatrix)mT.insert(mY.add(mG.multiply(mW)));
-			
-//			debugResult(mT, 0);
-//			if (true) return 0;	
-			
+			mT = (Basic2DMatrix) mT.insert(mY.add(mG.multiply(mW)));
+
+			// debugResult(mT, 0);
+			// if (true) return 0;
+
 			// System.out.println("Np : " + N_P);
 
-			double [] terms = new double[3];
+			double[] terms = new double[3];
 
-			Vector tmpVec = mP.transpose().multiply(BasicVector.constant(N, 1));
+			Vector tmpVec = mP.transpose().multiply(BasicVector.constant(M, 1));
 			tmpVec.update(INV2_FUNCTION);
 
-			terms[0] = (double)(mX.transpose().multiply(mP.transpose().multiply(BasicVector.constant(M, 1)).toDiagonalMatrix().multiply(mX)).trace());
-			terms[1] = (double)(mP.multiply(mX).transpose().multiply(mT).trace()) * (-2);
-			terms[2] = (double) mT.transpose().multiply(mP.multiply(BasicVector.constant(M, 1)).toDiagonalMatrix().multiply(mT)).trace();			
-			
+			terms[0] = (double) (mX.transpose()
+					.multiply(mP.transpose().multiply(BasicVector.constant(M, 1)).toDiagonalMatrix().multiply(mX))
+					.trace());
+			terms[1] = (double) (mP.multiply(mX).transpose().multiply(mT).trace()) * (-2);
+			terms[2] = (double) mT.transpose()
+					.multiply(mP.multiply(BasicVector.constant(N, 1)).toDiagonalMatrix().multiply(mT)).trace();
+
 			sigma2 = 0;
-			for (int i =0; i < terms.length; ++i){
+			for (int i = 0; i < terms.length; ++i) {
 				sigma2 += terms[i];
 				// System.out.println(terms[i]);
 			}
 
-			sigma2 /= (N_P*D);
+			sigma2 /= (N_P * D);
 			System.out.println(sigma2);
-			
+
 			// if (true) return 0;
 			addOverlay(imp);
 		}
 
 		// writeCSV();
 
-		// T is the transformation you are looking for 
+		// T is the transformation you are looking for
 		System.out.println("hello");
 		return 1;
 	}
 
-	// add parameters 
+	// add parameters
 	// matrix file name
 	// ugly function to read mx and mY from CSV
-	public void readCSV(){
+	public void readCSV() {
 
-		int N = 91;
-		int M = 91; 
-		int D = 2;
-		mX = new Basic2DMatrix(N, D);
-		mY = new Basic2DMatrix(M, D);
+		// int N = 112;
+		// int M = 133;
+		// int D = 2;
+		// mX = new Basic2DMatrix(N, D);
+		// mY = new Basic2DMatrix(M, D);
 
 		CSVReader reader = null;
-		String [] nextLine;
+		String[] nextLine;
 
 		try {
-			reader = new CSVReader(new FileReader("src/main/resources/x.csv"));
+			reader = new CSVReader(new FileReader("/Users/kkolyva/Desktop/cpd_ex/worm-folded.csv"), '\t');
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 
-		try {			
+		try {
 			int i = 0;
 			while ((nextLine = reader.readNext()) != null) {
 				mX.set(i, 0, Double.parseDouble(nextLine[0]));
@@ -336,11 +394,11 @@ public class CoherentPointDrift {
 		}
 
 		try {
-			reader = new CSVReader(new FileReader("src/main/resources/y.csv"));
+			reader = new CSVReader(new FileReader("/Users/kkolyva/Desktop/cpd_ex/worm-straight.csv"), '\t');
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		try {			
+		try {
 			int i = 0;
 			while ((nextLine = reader.readNext()) != null) {
 				mY.set(i, 0, Double.parseDouble(nextLine[0]));
@@ -353,10 +411,9 @@ public class CoherentPointDrift {
 
 	}
 
-	
-	public void writeCSV(){
+	public void writeCSV() {
 		CSVWriter writer = null;
-		String [] nextLine;
+		String[] nextLine;
 
 		try {
 			writer = new CSVWriter(new FileWriter("src/main/resources/xRes.csv"), '\t');
@@ -364,8 +421,8 @@ public class CoherentPointDrift {
 			e.printStackTrace();
 		}
 
-		try {			
-			for (int i = 0; i < N; ++i){
+		try {
+			for (int i = 0; i < N; ++i) {
 				nextLine = mX.getRow(i).toString().split(" ");
 				writer.writeNext(nextLine);
 			}
@@ -380,8 +437,8 @@ public class CoherentPointDrift {
 			e.printStackTrace();
 		}
 
-		try {			
-			for (int i = 0; i < N; ++i){
+		try {
+			for (int i = 0; i < N; ++i) {
 				nextLine = mY.getRow(i).toString().split(" ");
 				writer.writeNext(nextLine);
 			}
@@ -391,23 +448,8 @@ public class CoherentPointDrift {
 		}
 
 	}
-	
 
-
-	// y = G(m, :) * W
-	public void multVecMatrix(double[] G, double[] W, double [] y, int m){
-		for(int d = 0; d < D; ++d){
-			double res = 0;
-			for (int i = 0; i < M; ++i){
-				res += G[m*M + i]*W[i*D + d];
-			}
-			y[d] = res;
-		}
-	}
-
-
-
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		new ImageJ();
 		new CoherentPointDrift().runNonRigidRegistration(0);
 		// new CoherentPointDrift().readCSV();
--- src/main/java/coherent/point/drift/ManualPointSelection.java
+++ src/main/java/coherent/point/drift/ManualPointSelection.java
@@ -18,35 +18,45 @@ import ij.gui.Overlay;
 import ij.gui.PointRoi;
 import ij.gui.Roi;
 import mpicbg.imglib.util.Util;
+import net.imglib2.algorithm.stats.Normalize;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import util.ImgLib2Util;
 import util.opencsv.CSVWriter;
 
 /**
  * ManualPointSelection -- fast way to get the points manually 
- * and write them to txt after that
- * 
+ * and write them to cvs after that
  */
 
 public class ManualPointSelection {
 
+	// selected points
 	final ArrayList<double []> points =  new ArrayList<double[]>(1);
-
-	Img <FloatType> img;
-	MyMouseListener mouseListener;
-	MyKeyListener keyListener;
-	String dir = "/Users/kkolyva/Desktop/latest_desktop/";
-	File txtOutput = new File(dir + "init-1.csv");
+	final Img <FloatType> img;
+	final ImagePlus imp; // wrapper
+	final int numDimensions;
+	
+	// used for detection
+	final MyMouseListener mouseListener;
+	final MyKeyListener keyListener;
+	
+	// file names
+	String name =  "worm-folded";
+	String dir = "/Users/kkolyva/Desktop/cpd_ex/";
+	File output = new File(dir + name + ".csv");
+	File input = new File(dir + name + ".tif");
 
 	public ManualPointSelection(){
-		File file = new File(dir + "init-1.tif");
-		Img<FloatType> img = ImgLib2Util.openAs32Bit(file);
-		final ImagePlus imp = ImageJFunctions.wrapFloat(img, "");	
-
+		img = ImgLib2Util.openAs32Bit(input);
+		numDimensions = img.numDimensions();		
+		imp = ImageJFunctions.wrapFloat(img, "");
+		
 		imp.show();
-		mouseListener = new MyMouseListener(imp);
+		mouseListener = new MyMouseListener();
 		keyListener = new MyKeyListener();
 		imp.getCanvas().addMouseListener(mouseListener);
 		imp.getCanvas().addKeyListener(keyListener);	
@@ -55,41 +65,27 @@ public class ManualPointSelection {
 	public void addOverlay(ImagePlus imp){
 		Overlay overlay = imp.getOverlay();
 		if (overlay == null){
-			// System.out.println("addOverlay: overlay is null!");	
 			overlay = new Overlay();
 			imp.setOverlay( overlay );
 		}		
 		overlay.clear();
 		
 		// here should be the T coordinate of the fitted points
-		int numDimensions = 2;
-		double [] sigma = new double[]{5, 5};
 		double [] location = new double[numDimensions];
 		for (double[] point : points){
-			for (int d = 0; d < numDimensions; ++d){
-				location[d] = point[d];
-			} 
+			location = point.clone();
+			// special 
 			final PointRoi or = new PointRoi(location[0], location[1]);
-			or.setSize(100);
+			or.setStrokeColor(Color.RED);
 			overlay.add(or);
 		}
-
-//		imp.setOverlay(overlay);
-//		imp.updateAndDraw();	
 	}
 	
 
 	protected class MyMouseListener implements MouseListener
 	{
-		ImagePlus imp;
-		
-		public MyMouseListener(ImagePlus imp){
-			this.imp = imp;
-		}
-		
 		@Override
-		public void mouseClicked(MouseEvent e) {
-		}
+		public void mouseClicked(MouseEvent e) {}
 
 		@Override
 		public void mouseEntered(MouseEvent e) {}
@@ -103,14 +99,12 @@ public class ManualPointSelection {
 		@Override
 		public void mouseReleased( final MouseEvent e )
 		{
+			// convert the coordinates to "proper" coordinates
 			final int xm = imp.getCanvas().offScreenX( e.getX() );
 			final int ym = imp.getCanvas().offScreenY( e.getY() );
-			// imp.getCanvas().getMagnification()
-			
-			double [] location = new double[]{xm, ym};
-			points.add(location);
-			System.out.println("Added: ");
-			System.out.println("x: " + location[0] + " y: " + location[1]);
+		
+			points.add(new double[]{xm, ym});
+			System.out.println("Added:\n" + "x: " + xm + " y: " + ym);
 			addOverlay(imp);
 		}
 
@@ -118,13 +112,9 @@ public class ManualPointSelection {
 
 	protected class MyKeyListener implements KeyListener
 	{
-		public void keyTyped(KeyEvent e) {
-			// nothing
-		}
+		public void keyTyped(KeyEvent e) {}
 
-		public void keyPressed(KeyEvent e) {
-			// nothing
-		}
+		public void keyPressed(KeyEvent e) {}
 
 		public void keyReleased(KeyEvent e) {
 			int id = e.getID();	
@@ -132,27 +122,25 @@ public class ManualPointSelection {
 				char c = e.getKeyChar();
 				switch (c){
 				case 's':
-					savePointsToFile(txtOutput);
-					System.out.println("Saved: ");
-					System.out.println(points.size() + " Points");
-					break;
 				case 'f':
+				case 'q':
+					savePointsToFile(output);
+					System.out.println("Saved:\n" + points.size() + " Points");
 					break;
 				case 'r':
 				case 'd':	
 					if (points.size() - 1 > 0){
-						System.out.println("Removed: ");
-						System.out.println("x: " + points.get(points.size() - 1)[0] + " y: " + points.get(points.size() - 1)[1]);
+						System.out.println("Removed:\n" + "x: " + points.get(points.size() - 1)[0] + " y: " + points.get(points.size() - 1)[1]);
 						points.remove(points.size() - 1);
+						addOverlay(imp);
 					}
 					else{
 						// do nothing 
 					}
 					break;
 				default:
-					System.out.print("s: save file; \nr: delete last point");
+					System.out.println("s: save file; \nr: delete last point");
 				}
-
 			} 
 		}
 	}
