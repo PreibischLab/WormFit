54d1d3c8dd56f1dd525ee5bdbbadee82812b00f7
milkyklim <klim.kolyvanov@gmail.com>
2017-04-25 23:44:38 +0200

    fixed small bugs

--- src/main/java/klim/deconvolution/Preprocessing.java
+++ src/main/java/klim/deconvolution/Preprocessing.java
@@ -5,54 +5,65 @@ import java.io.File;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.real.FloatType;
 
-import deconvolution.AdjustInput;
-import mpicbg.imglib.wrapper.ImgLib2;
+import ij.ImageJ;
+import mpicbg.util.RealSum;
 import util.ImgLib2Util;
 
 public class Preprocessing {
 	public static void preprocessImage(){
-		// grab initial image 
-		// grab bg image 
-		
-		// make a z projection of the median filter and get an average value 
-		// median - average (get zero mean)
-		
-		// initial image - (median - average)
+		new ImageJ();
 		String path = "";
 		String file = "";
-		Img<FloatType> img = ImgLib2Util.openAs32Bit(new File(path + file + ".tif"));
-		Img<FloatType> bg  = ImgLib2Util.openAs32Bit(new File(path + "bg/" + file + "-m.tif"));
+		Img<FloatType> img = ImgLib2Util.openAs32Bit(new File("/Volumes/Samsung_T3/2017-04-25-beads/cropped/tif-14-c.tif"));//path + file + ".tif"));
+		Img<FloatType> bg  = ImgLib2Util.openAs32Bit(new File("/Volumes/Samsung_T3/2017-04-25-beads/cropped/bg/tif-14-c-m.tif"));//path + "bg/" + file + "-m.tif"));
 		
-		subtractAverage(bg);
-		subtractImgs(img, bg);
+		double average = getImageAverage(bg);	
+		System.out.println(average);
 		
+		subtractValue(bg, average);
+		subtractImg(img, bg);	
+		
+		ImageJFunctions.show(img);
 	}
-	
-	
-	public static void subtractAverage(Img<FloatType> bg){		
-		// compute average over all pixels
-		double sum = AdjustInput.sumImage(ImgLib2.wrapFloatToImgLib1(bg));
-		for (int d = 0; d < bg.numDimensions(); ++d)
-			sum /= bg.dimension(d);
 		
+	public static void subtractValue(Img<FloatType> bg, double value){			
 		Cursor<FloatType> cursor = bg.cursor();
 		
 		while(cursor.hasNext()){
-			cursor.fwd();		
-			cursor.get().sub(new FloatType((float)sum));
+			cursor.fwd();
+			float val = (float) (cursor.get().get() - value);
+			cursor.get().set(val);
 		}
 	}
 	
-	public static void subtractImgs(Img<FloatType> img, Img<FloatType> bg){
+	public static double getImageAverage(Img<FloatType> bg){
+		// compute average over all pixels
+		double sum = sumImage(bg);
+		for (int d = 0; d < bg.numDimensions(); ++d)
+			sum /= bg.dimension(d);
+		return sum;
+	}
+	
+	public static double sumImage(Img<FloatType> img)
+	{
+		final RealSum sum = new RealSum();		
+		for ( final FloatType t : img )
+			sum.add( t.get() );
+		return sum.getSum();
+	}
+	
+	public static void subtractImg(Img<FloatType> img, Img<FloatType> bg){
 		Cursor<FloatType> cursor = img.cursor();
 		RandomAccess<FloatType> ra = bg.randomAccess();
 		
 		while(cursor.hasNext()){
 			cursor.fwd();		
-			ra.setPosition(cursor);		
-			cursor.get().sub(ra.get());
+			ra.setPosition(cursor);			
+			float val = cursor.get().get() - ra.get().get();			
+			cursor.get().set(val);
 		}
 	}
 	
